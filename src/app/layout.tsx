ce5181a9067a5307cf356f38a467d1330de65cd8
dhanesh1232 <dhaneshreddy980@gmail.com>
2026-05-04 21:29:37 +0530

    feat: add Google Tag Manager tracking script and noscript fallback to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -189,10 +189,31 @@ export default function RootLayout({
             });
           `}
         </script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MN6R938B')`,
+          }}
+        />
       </head>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} ${jetbrainsMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MN6R938B"
+            height="0" width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>`,
+          }}
+        />
+
         {/* Skip to main content link for keyboard navigation */}
         <a href="#main-content" className="skip-link">
           Skip to main content
